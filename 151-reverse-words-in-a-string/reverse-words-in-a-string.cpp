6dcaded455003a5e6c7b9c59794ec385d80440ab
racheliee <146931361+racheliee@users.noreply.github.com>
2024-07-12 23:29:31 +0900

    Time: 3 ms (81.60%) | Memory: 10.7 MB (33.69%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
         string rev = "";
 
         std::istringstream ss(s);
         std::string word;
         vector<string> v;
 
-        while(ss >> word)
-        {
+        while (ss >> word) {
             v.push_back(word);
         }
 
         reverse(v.begin(), v.end());
-        
         int sz = v.size();
-        for(int i = 0; i < sz; ++i){
+
+        for (int i = 0; i < sz; ++i) {
             rev += v[i];
-            if(i != sz-1) rev += " ";
+            if (i != sz - 1)
+                rev += " ";
         }
-        
+
         return rev;
     }
 };
