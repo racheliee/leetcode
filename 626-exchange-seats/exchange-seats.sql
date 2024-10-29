4dc08e8b9bbfe806fabd970e515d755f3ce11840
racheliee <146931361+racheliee@users.noreply.github.com>
2024-10-29 14:34:15 -0700

    Time: 414 ms (34.70%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
 select 
-    case
-        when id % 2 = 0 then id - 1 # even
-        when id % 2 = 1 and id < (select max(id) from seat) then id + 1 # odd
-        else id # last elem
-    end as id,
-    student
+    id,
+    case 
+        when id % 2 = 0 then lag(student) over(order by id)
+        else coalesce(lead(student) over (order by id), student)
+    end as student
 from seat
 order by id asc
