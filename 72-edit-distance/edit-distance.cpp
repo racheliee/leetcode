7d0dea7462cfd02f15511d3d5536fabfa2822c4d
racheliee <146931361+racheliee@users.noreply.github.com>
2024-10-29 16:22:28 -0700

    Time: 8 ms (41.85%) | Memory: 13.5 MB (20.41%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int editDistance(string& w1, string& w2, int i, int j, vector<vector<int>>& memo){
+        if(i == 0) return j;
+        if(j == 0) return i;
+
+        if(memo[i][j] != -1) return memo[i][j];
+
+        if(w1[i-1] == w2[j-1])
+            memo[i][j] = editDistance(w1, w2, i-1, j-1, memo);
+        else    
+            memo[i][j] = 1 + min({editDistance(w1, w2, i-1, j-1, memo), editDistance(w1, w2, i-1, j, memo), editDistance(w1, w2, i, j-1, memo)});
+        
+        return memo[i][j];
+    }
+
+    int minDistance(string word1, string word2) {
+        int l1 = (int)word1.length();
+        int l2 = (int)word2.length();
+        vector<vector<int>> memo(l1+1, vector<int> (l2+1, -1));
+        return editDistance(word1, word2, l1, l2, memo);
+    }
+};
