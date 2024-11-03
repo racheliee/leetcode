fcb73e71ec344897d2f0be798ed19192a40b05f0
racheliee <146931361+racheliee@users.noreply.github.com>
2024-11-03 15:10:40 -0800

    Time: 95 ms (8.76%) | Memory: 67.3 MB (10.16%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void getCombination(vector<int>& candidates, int start, int len, int target, int val, vector<vector<int>>& ret, vector<int> curr){
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ret;
+        getCombination(candidates, 0, target, 0, ret, {});
+        return ret;
+    }
+
+private:
+    void getCombination(vector<int>& candidates, int start, int target, int val, vector<vector<int>>& ret, vector<int> curr){
         if(target == val){
             ret.push_back(curr);
             return;
         }
-        if(target < val){
+        if(target < val || start >= candidates.size()){
             return;
         }
 
     
-        for(int i = start; i < len; ++i){
-            curr.push_back(candidates[i]);
-            getCombination(candidates, i, len, target, val + candidates[i], ret, curr);
-            curr.pop_back();
-        }
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ret;
-        getCombination(candidates, 0, (int)candidates.size(), target, 0, ret, {});
-        return ret;
+        curr.push_back(candidates[start]);
+        getCombination(candidates, start, target, val + candidates[start], ret, curr);
+        curr.pop_back();
+        getCombination(candidates, start + 1, target, val, ret, curr);
     }
 };
