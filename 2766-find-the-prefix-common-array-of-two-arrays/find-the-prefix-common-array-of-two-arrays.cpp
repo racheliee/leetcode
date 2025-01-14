aa6b01874e060266c87ef28eae4ca11f8f38a6c0
rachel park <146931361+racheliee@users.noreply.github.com>
2025-01-14 01:54:21 -0800

    Time: 8 ms (53.56%) | Memory: 88.3 MB (41.68%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = (int)A.size();
-        vector<unordered_map<int, int>> v(n);
-        
-        ++v[0][A[0]];
-        ++v[0][B[0]];
-        for(int i = 1; i < n; ++i){
-            v[i] = v[i-1];
-            ++v[i][A[i]];
-            ++v[i][B[i]];
-        }
 
+        unordered_map<int, int> m;
         vector<int> ret(n);
         int cnt = 0;
-        for(int i = 0; i < n; ++i){
-            // cout << "index: " << i << endl;
-            for(auto it = v[i].begin(); it != v[i].end(); ++it){
-                // cout << "num " << it->first << " occurs " << it->second << endl;
-                if(it->second == 2) ++cnt;
+        for (int i = 0; i < n; ++i) {
+            ++m[A[i]];
+            ++m[B[i]];
+            for (auto it = m.begin(); it != m.end(); ++it) {
+                // cout << "num " << it->first << " occurs " << it->second <<
+                // endl;
+                if (it->second == 2)
+                    ++cnt;
             }
             ret[i] = cnt;
             cnt = 0;
