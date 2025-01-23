e7bac79dd7c3c41cc849083f2f988d3b2c7d4502
rachel park <146931361+racheliee@users.noreply.github.com>
2025-01-23 04:46:15 -0800

    Time: 0 ms (100.00%) | Memory: 26.8 MB (39.90%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int i, int j){
-        int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-
-        int ret = 1;
-        q.push({i, j});
-        visited[i][j] = true;
-
-        while(!q.empty()){
-            auto [x, y] = q.front();
-            q.pop();
-
-            for(int i = 0; i < m; ++i){
-                if(visited[i][y] == false && grid[i][y] == 1 && i != x){
-                    ++ret;
-                    visited[i][y] = true;
-                    q.push({i, y});
-                }
-            }
-
-            for(int j = 0; j < n; ++j){
-                if(visited[x][j] == false && grid[x][j] == 1 && j != y){
-                    ++ret;
-                    visited[x][j] = true;
-                    q.push({x, j});
-                }
-            }
-        }
-
-        return ret == 1 ? 0 : ret;
-    }
-
     int countServers(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
 
         int ret = 0;
         for(int i = 0; i < m; ++i){
             for(int j = 0; j < n; ++j){
-                if(grid[i][j] && visited[i][j] == false){
-                    ret +=  bfs(grid, visited, i, j);
+                if(grid[i][j]){
+                    ++row[i];
+                    ++col[j];
                 }
+            }
+        }
+
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j] && (row[i] > 1 || col[j] > 1))
+                    ++ret;
             }
         }
 
