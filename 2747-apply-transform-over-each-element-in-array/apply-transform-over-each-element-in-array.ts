c82f167a5e1117df08df6376a844065332934c1a
rachel park <146931361+racheliee@users.noreply.github.com>
2025-02-04 17:43:35 +0900

    Time: 66 ms (10.38%) | Memory: 51.2 MB (81.32%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
@@ -1,10 +1,9 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    let ret: number[] = [];
     const len: number = arr.length;
 
     for(let i = 0; i < len; ++i){
-        ret.push(fn(arr[i], i));
+        arr[i] = fn(arr[i], i);
     }
 
-    return ret;
+    return arr;
 };
