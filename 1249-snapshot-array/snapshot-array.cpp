1c5379bb14a6e6f02a234c5059474f6e0171c264
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-09 03:44:19 +0900

    Time: 79 ms (61.37%) | Memory: 186.9 MB (38.03%) - LeetSync

---
+++ 1249-snapshot-array/snapshot-array.cpp
@@ -0,0 +1,36 @@
+class SnapshotArray {
+public:
+    SnapshotArray(int length) {
+        snap_id = 0;
+        snaps.clear();
+        snaps.resize(length);
+    }
+    
+    void set(int index, int val) {
+        snaps[index][snap_id] = val;
+    }
+    
+    int snap() {
+        ++snap_id;
+        return snap_id - 1;
+    }
+    
+    int get(int index, int snap_id) {
+        auto &m = snaps[index];
+        auto it = m.upper_bound(snap_id);
+        if (it == m.begin()) return 0;
+        return (--it)->second;
+    }
+
+private:
+    vector<map<int, int>> snaps; // snaps[index] = snap_id, val
+    int snap_id;
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
