ef554f4b1143807cc79603f88bfd049bbcc008f7
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-13 21:25:52 +0900

    Time: 8 ms (34.92%) | Memory: 13.9 MB (35.17%) - LeetSync

---
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long countVowels(string word) {
+        int len = word.length();
+        string vowels = "aeiou";
+        long long ret = 0;
+
+        for(int i = 0; i < len; ++i){
+            if(vowels.find(word[i]) == string::npos)
+                continue;
+            
+            ret += ((long long)i+1)*(len-i);
+        }
+
+        return ret;
+    }
+};
