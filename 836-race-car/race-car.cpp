04dc0e09d6236d834d0766b06b2be5f14966f8ad
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-15 20:37:15 +0900

    Time: 15 ms (50.57%) | Memory: 13.2 MB (46.93%) - LeetSync

--- 836-race-car/race-car.cpp
+++ 836-race-car/race-car.cpp
@@ -1,45 +1,33 @@
-
 class Solution {
-  public:
-    typedef struct race_car {
-        long long pos;
-        long long speed;
-        int moves;
-
-        bool operator==(const race_car& other) const {
-            return pos == other.pos && speed == other.speed;
-        }
-    };
-
-    struct race_car_hash {
-        size_t operator()(const race_car& rc) const {
-            return hash<int>()(rc.pos) ^ (hash<int>()(rc.speed) << 1);
-        }
-    };
-
+public:
     int racecar(int target) {
-        queue<race_car> q;
+        queue<pair<pair<long long, long long>, int>> q; // pos, speed, moves
+        q.push({{0, 1}, 0});
 
-        q.push({0, 1, 0});
-
-        unordered_set<race_car, race_car_hash> visited;
+        set<pair<long long, long long>> visited;
 
         while (!q.empty()) {
-            race_car curr = q.front();
+            auto [curr, moves] = q.front();
             q.pop();
 
-            if(curr.pos == target)
-                return curr.moves;
-            
-            if(visited.find(curr) != visited.end())
+            if (curr.first == target)
+                return moves;
+
+            if (visited.find(curr) != visited.end())
                 continue;
-
             visited.insert(curr);
-            
-            q.push({curr.pos + curr.speed, curr.speed * 2, curr.moves + 1});
 
-            if((curr.pos + curr.speed > target && curr.speed > 0) || (curr.pos + curr.speed < target && curr.speed < 0)){
-                q.push({curr.pos, curr.speed < 0 ? 1 : -1, curr.moves + 1});
+            long long pos = curr.first, speed = curr.second;
+
+            // case: 'A'
+            q.push({{pos + speed, speed * 2}, moves + 1});
+
+            // case: 'R'
+            // only needed when we're below target and speed is negative
+            // and when we're above target and speed is positive
+            if ((pos + speed < target && speed < 0) ||
+                (pos + speed > target && speed > 0)) {
+                q.push({{pos, speed < 0 ? 1 : -1}, moves + 1});
             }
         }
 
