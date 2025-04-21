4552124ab8a9096d31941bd2de6e2c9dcee99940
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-22 04:45:23 +0900

    Time: 8 ms (25.87%) | Memory: 18.4 MB (53.74%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool dfs(int curr, vector<int>& ret, vector<int>& visited, const vector<vector<int>>& adj) {
+        if (visited[curr] == 2)
+            return true; // already visited
+
+        visited[curr] = 1; // visiting
+        for (auto i : adj[curr]) {
+            if (visited[i] == 1)
+                return false; // cycle
+            else if (visited[i] == 2)
+                continue; // visited
+
+            if (!dfs(i, ret, visited, adj))
+                return false;
+        }
+        visited[curr] = 2; // visited
+        ret.push_back(curr);
+        return true;
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        for (int i = 0; i < prerequisites.size(); ++i) { // be careful with loop termination
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+
+        vector<int> ret;
+        vector<int> visited(numCourses, 0);
+        for (int i = 0; i < numCourses; ++i) {
+            if (!dfs(i, ret, visited, adj))
+                return {};
+        }
+
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
