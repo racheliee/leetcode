12c5697d2794d5d682770a918c9351ec8fa73d16
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-22 23:36:28 +0900

    Time: 106 ms (30.93%) | Memory: 182.1 MB (41.14%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,43 +1,38 @@
-
 class LRUCache {
-private:
-    int cap;
-    list<int> lru; // stores keys, most recent at front
-    unordered_map<int, pair<int, list<int>::iterator>> cache; // key -> {val, lru iterator}
-
 public:
-    LRUCache(int capacity) {
+    LRUCache(int capacity): cache() {
         cap = capacity;
     }
-
+    
     int get(int key) {
-        if (cache.find(key) == cache.end())
+        if(cache.find(key) == cache.end())
             return -1;
-
-        // move accessed key to front of LRU list
+        
         lru.erase(cache[key].second);
         lru.push_front(key);
         cache[key].second = lru.begin();
 
         return cache[key].first;
     }
-
+    
     void put(int key, int value) {
-        if (cache.find(key) != cache.end()) {
-            // key exists, update value and move to front
+        if(cache.find(key) != cache.end())
             lru.erase(cache[key].second);
-        } else if (cache.size() == cap) {
-            // cache full, evict least recently used (back)
-            int lru_key = lru.back();
+        else if (cache.size() == cap) {
+            cache.erase(lru.back());
             lru.pop_back();
-            cache.erase(lru_key);
         }
 
-        // insert new key at front
         lru.push_front(key);
         cache[key] = {value, lru.begin()};
     }
+
+private:
+    list<int> lru;
+    int cap;
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
 };
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
