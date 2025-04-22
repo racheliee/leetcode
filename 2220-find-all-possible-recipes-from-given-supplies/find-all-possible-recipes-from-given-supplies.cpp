4d6ad14bc8624689c1858b86baf45c87c6c49ab6
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-22 17:04:58 +0900

    Time: 53 ms (75.29%) | Memory: 124.6 MB (29.77%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    bool dfs(string food, const unordered_map<string, vector<string>>& adj, unordered_set<string>& supply_set, unordered_map<string, int>& visited) {
-        cout << "food: " << food << endl;
-        if(supply_set.find(food) != supply_set.end())            
+    bool dfs(string food, const unordered_map<string, vector<string>>& adj,
+             unordered_set<string>& supply_set,
+             unordered_map<string, int>& visited) {
+
+        if (supply_set.find(food) != supply_set.end())
             return true;
         else {
-            if(adj.find(food) == adj.end()) // not in supply or recipes; impossible
+            if (adj.find(food) ==
+                adj.end()) // not in supply or recipes; impossible
                 return false;
         }
-        
-        if(visited[food] == 2)
+
+        if (visited[food] == 2)
             return true; // visited
 
-        if(visited[food] == 1)
+        if (visited[food] == 1)
             return false; // cycle
 
         visited[food] = 1;
 
-        for(auto ingred: adj.at(food)) {
-            cout << ingred << endl;
-            if(!dfs(ingred, adj, supply_set, visited)) 
+        for (auto ingred : adj.at(food)) {
+
+            if (!dfs(ingred, adj, supply_set, visited))
                 return false;
         }
 
@@ -28,19 +31,26 @@ public:
         return true;
     }
 
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_set<string> supply_set = unordered_set(supplies.begin(), supplies.end());
+    vector<string> findAllRecipes(vector<string>& recipes,
+                                  vector<vector<string>>& ingredients,
+                                  vector<string>& supplies) {
+        unordered_set<string> supply_set =
+            unordered_set(supplies.begin(), supplies.end());
         unordered_map<string, vector<string>> adj;
         vector<string> pos_recipes;
         unordered_map<string, int> visited;
 
         int len = ingredients.size();
-        for(int i = 0; i < len; ++i) {
+        for (int i = 0; i < len; ++i) {
             adj[recipes[i]] = ingredients[i];
         }
 
-        for(string food: recipes) {
-            if(dfs(food, adj, supply_set, visited))
+        for (string food : recipes) {
+            if (supply_set.find(food) != supply_set.end()) {
+                pos_recipes.push_back(food);
+                continue;
+            }
+            if (dfs(food, adj, supply_set, visited))
                 pos_recipes.push_back(food);
         }
 
