281714dd0c87fafbfbc8371cbcd88d3756d5a832
rachel park <146931361+racheliee@users.noreply.github.com>
2025-04-22 22:51:40 +0900

    Time: 198 ms (16.85%) | Memory: 144.1 MB (18.76%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.cpp
@@ -0,0 +1,38 @@
+class DetectSquares {
+public:
+    DetectSquares() : coor() {}
+
+    void add(vector<int> point) { ++coor[point[0]][point[1]]; }
+
+    int count(vector<int> point) {
+        int x = point[0];
+        int y = point[1];
+
+        int ret = 0;
+
+        if (coor.count(x) == 0) // no coordinates on the same x-axis
+            return 0;
+
+        for (auto& [x2, y_coor] : coor) {
+            if (x2 == x)
+                continue;
+
+            int d = abs(x2 - x);
+
+            ret += y_coor[y] * y_coor[y + d] * coor[x][y + d];
+            ret += y_coor[y] * y_coor[y - d] * coor[x][y - d];
+        }
+
+        return ret;
+    }
+
+private:
+    unordered_map<int, unordered_map<int, int>> coor;
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
