54abfd9c4096730e80ff52f9ed382c2fb9385db6
rachel park <146931361+racheliee@users.noreply.github.com>
2025-07-01 01:35:00 +0900

    Time: 0 ms (100.00%) | Memory: 28.1 MB (85.14%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0, candidate = 0;
+
+        for (int num : nums) {
+            if (cnt == 0)
+                candidate = num;
+            cnt += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
