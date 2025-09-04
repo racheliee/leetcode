5770de8ceeb4f8355ec156118950d090f647c29f
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-05 01:37:38 +0900

    Time: 3 ms (9.92%) | Memory: 29.6 MB (61.38%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,9 +2,20 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int len = nums.size();
-        vector<int> temp(len);
-        for(int i = 0; i < len; ++i) 
-            temp[(i+k)%len] = nums[i];
-        nums = temp;
+        // reverse the array!!
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k % len);
+        reverse(nums.begin() + k % len, nums.end());
     }
 };
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int len = nums.size();
+//         vector<int> temp(len);
+//         for(int i = 0; i < len; ++i) 
+//             temp[(i+k)%len] = nums[i];
+//         nums = temp;
+//     }
+// };
