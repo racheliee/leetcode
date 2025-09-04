81a8c2be5c0488698c543cc31bb111b493fb47ba
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-05 02:20:33 +0900

    Time: 2 ms (29.40%) | Memory: 9.2 MB (32.29%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+
+        if(a.length() < b.length())
+            swap(a, b);
+        
+        int alen = a.length(), blen = b.length();
+        while(b.length() < alen) 
+            b += '0';
+
+        cout << a << endl << b << endl;
+
+        int carry = 0;
+        string ret = "";
+        for(int i = 0; i < alen; ++i) {
+            if(a[i] == '1' && b[i] == '1') {
+                ret += carry == 1 ? '1' : '0';
+                carry = 1;
+            }else if(a[i] == '0' && b[i] == '0') {
+                ret += carry == 1 ? '1' : '0';
+                carry = 0;
+            } else {
+                ret += carry == 1? '0' : '1';
+                carry = carry == 1? 1 : 0;
+            }
+        }
+
+        if (carry)
+            ret += '1';
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
