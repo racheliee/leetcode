fddd7afd521ac6184ffae8b4de28fbc356c00daa
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-05 02:26:25 +0900

    Time: 1 ms (33.43%) | Memory: 10.5 MB (24.49%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -4,19 +4,19 @@ public:
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
 
-        if(a.length() < b.length())
+        if (a.length() < b.length())
             swap(a, b);
-        
+
         int alen = a.length(), blen = b.length();
-        while(b.length() < alen) 
+        while (b.length() < alen)
             b += '0';
 
         int carry = 0;
         string ret = "";
-        for(int i = 0; i < alen; ++i) {
+        for (int i = 0; i < alen; ++i) {
             int num = (a[i] - '0') + (b[i] - '0') + carry;
             ret += to_string(num & 1);
-            carry = num & 2 ? 1: 0;
+            carry = num & 2 ? 1 : 0;
 
             // if(a[i] == '1' && b[i] == '1') {
             //     ret += carry == 1 ? '1' : '0';
