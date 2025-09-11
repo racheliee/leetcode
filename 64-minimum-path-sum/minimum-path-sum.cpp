eb6a0327a1b7061ff2b7645bb92db9a79517dee9
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-11 19:13:25 +0900

    Time: 0 ms (100.00%) | Memory: 15.1 MB (98.67%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,4 +1,24 @@
 class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) grid[i][j] += grid[i][j-1];
+                else if (j == 0) grid[i][j] += grid[i-1][j];
+                else grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+};
+
+
+/* dfs solution
+class Solution {
 public:    
     bool is_oob(int x, int y, int m, int n) {
         if(x < 0 || x >= m || y < 0 || y >= n)
@@ -40,3 +60,4 @@ private:
     int dx[2] = {0, 1};
     int dy[2] = {1, 0};
 };
+*/
