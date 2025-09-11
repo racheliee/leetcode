d5cd409c0516ab2546bb8fcb7f6d7b8b97a4a9da
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-11 17:45:21 +0900

    Time: 12 ms (69.47%) | Memory: 16.6 MB (46.45%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -6,6 +6,39 @@ public:
             return true;
         return false;
     }
+
+    string sortVowels(string s) {
+        unordered_map<char, int> count;
+
+        for(const char& c: s)
+            ++count[c];
+        
+        string sorted = "AEIOUaeiou";
+        int idx = 0, len = s.length();
+
+        for(int i = 0; i < len; ++i) {
+            if(idx >= 10) break;
+            if(!isVowel(s[i])) 
+                continue;
+            while(count[sorted[idx]] == 0)
+                ++idx;
+            s[i] = sorted[idx];
+            --count[sorted[idx]];
+        }
+
+        return s;
+    }
+};
+
+/* sorting
+class Solution {
+public:
+    bool isVowel(char c) {
+        if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||
+            c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
+            return true;
+        return false;
+    }
     string sortVowels(string s) {
         vector<int> indices;
         vector<char> vowels;
@@ -24,13 +57,13 @@ public:
         int idx = 0;
         int vlen = indices.size();
         for (int i = 0; i < len; ++i) {
-            if (idx < vlen && i == indices[idx]) {
+            if (idx < vlen && i == indices[idx])
                 ret += vowels[idx++];
-            } else {
+            else 
                 ret += s[i];
-            }
         }
 
         return ret;
     }
 };
+*/
