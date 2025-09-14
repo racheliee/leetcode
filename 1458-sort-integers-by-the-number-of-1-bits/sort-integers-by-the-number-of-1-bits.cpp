cce7d76ffa46a06a3b9b630668a0873181e59db7
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-15 02:44:40 +0900

    Time: 0 ms (100.00%) | Memory: 14.2 MB (78.08%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    static int num1Bits(int n) {
-        string binary = bitset<14>(n).to_string();
-        int cnt = 0;
-        for(const auto& c: binary) cnt = c == '1' ? cnt + 1 : cnt;
-        return cnt;
-    }
-
-    static bool customCompare(const int& a, const int& b) {
-        int an = num1Bits(a), bn = num1Bits(b); 
-        if(an == bn) return a < b;
-        return an < bn;
-    }
-
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), customCompare);
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            int an = __builtin_popcount(a), bn = __builtin_popcount(b);
+            return an == bn ? a < b : an < bn;
+        });
         return arr;
     }
 };
