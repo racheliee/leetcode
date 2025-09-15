f68ecdfab1a1509b8588f23e3202a15bf7ba39af
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-16 02:26:24 +0900

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.16%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -9,3 +9,17 @@ public:
         return res;
     }
 };
+
+/*
+1. if XOR-ed with 0, return same number: 5^ 0 = 5
+
+2. if XOR w same number, return 0: 5^5 = 0;
+
+3. XOR 2 diff numbers, return nothing: 4 ^ 5 = 5 ^ 4
+
+SO basically,
+O ^ O ^ 1 ^ 1 ^ 2 ^ 3 ^ 3
+everything cancels out but 2
+
+ot you can use gaussian sum
+*/
