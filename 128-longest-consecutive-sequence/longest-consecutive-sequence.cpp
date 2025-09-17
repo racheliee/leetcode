464e4ff537c61a558795bcd222cf4378485a302c
rachel park <146931361+racheliee@users.noreply.github.com>
2025-09-17 17:35:35 +0900

    Time: 81 ms (61.48%) | Memory: 89 MB (19.09%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> s(nums.begin(), nums.end());
         int ret = 0;
 
-        for(const auto& n: s) {
+        for(const auto& n: s) { //iterate the set, not the nums
             // if there is an early start, skip
             if(s.find(n-1) != s.end()) continue;
 
